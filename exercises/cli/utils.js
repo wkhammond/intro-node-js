654b3a0cb5271762769827c1137775265717302c
i721018 <Will.Hammond@voya.com>
2019-07-29 16:44:26 -0400

    Second exercise, CLIs: installing packages and file system stuff

--- exercises/cli/utils.js
+++ exercises/cli/utils.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 // this path needs to be relative to work with fs
-const contactsLocation = 'contacts.json'
+const contactsLocation = path.join(__dirname, './contacts.json')
 
 /**
  * should read the contacts at the
@@ -10,7 +10,7 @@ const contactsLocation = 'contacts.json'
  * it to a js object
  */
 const getContacts = () => {
-  
+  return JSON.parse(fs.readFileSync(contactsLocation))
 }
 
 /**
@@ -19,7 +19,7 @@ const getContacts = () => {
  * @param {Object} contacts contacts object
  */
 const saveContacts = (contacts) => {
-
+  fs.writeFileSync(contactsLocation, JSON.stringify(contacts, null, 2))
 }
 
 module.exports = {
